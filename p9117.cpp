a224e834deb1e663dc71ecdd6f3a2cccb2fc3406
Mark <bombhero@users.noreply.github.com>
2023-10-26 13:26:10 +0800

    Add p9117

--- p9117.cpp
+++ p9117.cpp
@@ -1,33 +1,32 @@
-// Not finish yet.
+// Not finish yet. 3 TLE
 #include <iostream>
 #include <cstring>
 using namespace std;
 
-#define T 10
-#define N 100001
+#define N 10000002
 
 struct PaintSize{
     int n;
     int m;
 };
 
-unsigned int *paint[T];
-struct PaintSize paint_size[T];
+unsigned int paint[N];
+struct PaintSize paint_size;
 
-void op0(int t_idx, int x, int c)
+void op0(int x, int c)
 {
     int i;
 
-    for(i=1; i<=paint_size[t_idx].m; i++)
-        paint[t_idx][x][i] = c;
+    for(i=0; i<paint_size.m; i++)
+        paint[x*paint_size.m+i] = c;
 }
 
-void op1(int t_idx, int x, int c)
+void op1(int x, int c)
 {
     int i;
 
-    for(i=1; i<=paint_size[t_idx].n; i++)
-        paint[t_idx][i][x] = c;
+    for(i=0; i<paint_size.n; i++)
+        paint[i*paint_size.m+x] = c;
 }
 
 int main()
@@ -36,33 +35,30 @@ int main()
     int i, op;
     int t_idx, r_idx, c_idx;
 
-    memset(paint, 0, T*N*N*sizeof(unsigned int));
-
     cin>>t;
     for(t_idx=0; t_idx<t; t_idx++){
-        cin>>paint_size[t_idx].n>>paint_size[t_idx].m>>q;
+        cin>>paint_size.n>>paint_size.m>>q;
+        memset(paint, 0, (paint_size.n*paint_size.m*sizeof(unsigned int)));
         for(i=0; i<q; i++){
             cin>>op>>x>>c;
             switch (op)
             {
                 case 0:
-                    op0(t_idx, x, c);
+                    op0(x-1, c);
                     break;
                 case 1:
-                    op1(t_idx, x, c);
+                    op1(x-1, c);
                     break;
                 default:
                     break;
             }
         }
-    }
 
-    for(t_idx=0; t_idx<t; t_idx++){
-        for(r_idx=1; r_idx<=paint_size[t_idx].n; r_idx++){
-            for(c_idx=1; c_idx<=paint_size[t_idx].m; c_idx++){
-                cout<<paint[t_idx][r_idx][c_idx];
-                if(c_idx==paint_size[t_idx].m)
-                    cout<<endl;
+        for(r_idx=0; r_idx<paint_size.n; r_idx++){
+            for(c_idx=0; c_idx<paint_size.m; c_idx++){
+                cout<<paint[r_idx*paint_size.m + c_idx];
+                if(c_idx == (paint_size.m-1))
+                    printf("\n");
                 else
                     cout<<" ";
             }
