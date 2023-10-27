f14d7bcfeaed857d0d0db909682588404ee38bae
Mark <bombhero@users.noreply.github.com>
2023-10-27 16:13:39 +0800

    Finish p9755 with 2 AC

--- p9755.cpp
+++ p9755.cpp
@@ -1,19 +1,24 @@
-// Not finish yet
+// Not finish yet. 2AC
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #define N 100002
+// #define DEBUG
 
 struct Node{
     unsigned long long a;
     unsigned int b;
     int c;
-    int tall;
+    unsigned long long tall;
+};
+
+struct Path{
+    int v;
+    int w;
 };
 
 struct Node node_list[N]; // from 1
-int plant_list[N]; // from 1
-int path_list[N][2]; // from 1
 
 int growup_one_day(int x, struct Node node)
 {
@@ -42,8 +47,9 @@ int calc_grow_days(int current_x, struct Node node)
 int main()
 {
     int n, i, x;
-    int next_count;
-    int next_node_list[N];
+    int paint_id, remained_x, max_paint_id, max_remained_x;
+    bool done=true;
+    vector<struct Path> path_vec;
 
     cin>>n;
     for(i=1; i<=n; i++){
@@ -51,13 +57,62 @@ int main()
         node_list[i].tall = 0;
     }
     for(i=1; i<n; i++){
-        cin>>path_list[i][0]>>path_list[i][1];
+        struct Path new_path;
+        cin>>new_path.v>>new_path.w;
+        path_vec.push_back(new_path);
     }
-    next_count = 1;
-    next_node_list[1]=1;
 
-    for(x=1; ;x++){
-    }    
+    node_list[1].tall += growup_one_day(1, node_list[1]);
+    for(x=2; ;x++){
+        done = true;
+        max_paint_id = 0;
+        max_remained_x = 0;
+        for(i=0; i<path_vec.size(); i++){
+            if(node_list[path_vec[i].v].tall>0 && node_list[path_vec[i].w].tall==0){
+                paint_id = path_vec[i].w;
+                remained_x = calc_grow_days(x, node_list[path_vec[i].w]);
+            }else if(node_list[path_vec[i].v].tall==0 && node_list[path_vec[i].w].tall>0){
+                paint_id = path_vec[i].v;
+                remained_x = calc_grow_days(x, node_list[path_vec[i].v]);
+            }else{
+                continue;
+            }
+
+            if(max_remained_x < remained_x){
+                max_paint_id = paint_id;
+                max_remained_x = remained_x;
+            }
+        }
+
+#ifdef DEBUG
+        cout<<"Day "<<x<<": ";
+#endif
+        for(i=1; i<=n; i++){
+            if(i==max_paint_id || node_list[i].tall>0){
+                node_list[i].tall += growup_one_day(x, node_list[i]);
+            }
+#ifdef DEBUG
+            cout<<node_list[i].tall<<" ";
+#endif
+            if(node_list[i].tall < node_list[i].a)
+                done = false;
+        }
+#ifdef DEBUG
+        cout<<endl;
+#endif
+
+        if(done)
+            break;
+
+        for(i=0; i<path_vec.size(); i++){
+            if(node_list[path_vec[i].v].tall>0 && node_list[path_vec[i].w].tall>0){
+                path_vec.erase(path_vec.begin() + i);
+                break;
+            }
+        }
+    }
+
+    cout<<x;
 
     return 0;
 }
